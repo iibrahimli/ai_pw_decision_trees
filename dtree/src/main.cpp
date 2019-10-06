e719a46fd74cef6988bdd6b8d682a755c0fcc268
iibrahimli <imranibrahimli98@gmail.com>
2019-10-06 21:56:32 +0400

    test functions

--- dtree/src/main.cpp
+++ dtree/src/main.cpp
@@ -21,6 +21,9 @@ int main(int argc, char * argv[]){
 
     std::string data_filename = argv[1];
 
+
+    // read and parse data
+
     auto t1 = std::chrono::high_resolution_clock::now();
 
     parser ps;
@@ -34,6 +37,18 @@ int main(int argc, char * argv[]){
     for(auto [id, name] : id_to_label) cout << id << ": " << name << endl;
 
     cout << endl << dataset.size() << " samples parsed in " << fixed << setprecision(2) << (float) duration / 1000 << " ms" << endl;
+
+
+    // DEBUG
+    cout << endl;
+    cout << "# of labels: " << num_labels(dataset) << endl;
+    cout << "# of samples per label: " << num_per_label(dataset) << endl;
+    cout << "dataset entropy: " << dataset_entropy(dataset) << endl;
+
+
+    // build decision tree
+    
+
 
     return 0;
 }
