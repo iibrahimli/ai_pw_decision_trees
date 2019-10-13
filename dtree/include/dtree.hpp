8eca287425df8a01316357ab290b2c60f950a53e
iibrahimli <imranibrahimli98@gmail.com>
2019-10-13 23:35:07 +0400

    night push

--- dtree/include/dtree.hpp
+++ dtree/include/dtree.hpp
@@ -9,30 +9,88 @@
 namespace dt{
 
 
-enum node_type : bool {
+enum node_type {
     BRANCH,
     LEAF
 };
 
 
 struct decision_tree_node {
+
     node_type                           type;         // type of node
-    std::size_t                         i_feat;       // index of feature to consider
-    std::size_t                         prediction;   // prediction (class_id) if it is a LEAF node
+    std::size_t                         feat_id;      // index of feature to consider
+    std::size_t                         group_id;     // group in i_feat to consider
+    std::size_t                         pred;         // prediction (class_id) if it is a LEAF node
     std::vector<decision_tree_node *>   branches;     // branches corresponding to each group
+
+
+    decision_tree_node()
+        :   type     {LEAF},
+            feat_id  {0},
+            group_id {0},
+            pred     {0}
+    {}
+
+
+    decision_tree_node(node_type tp, std::size_t feat_i = 0, std::size_t group_i = 0, std::size_t prediction = 0)
+        :   type     {tp},
+            feat_id  {feat_i},
+            group_id {group_i},
+            pred     {prediction}
+    {}
+
 };
 
 
 struct decision_tree{
 
+private:
+
+    // root node
     decision_tree_node * root;
+
+    // number of groups for all features (assumend to be the same)
+    std::size_t          n_groups;
+
+
+    // stopping condition threshold
+    float                sc_threshold;
+
 
 
     /*
-        default constructor
+        is it time to stop?
+        evaluates stopping condition on a GROUP (give it a group NOT the whole dataset)
     */
-    decision_tree()
-    :   root {nullptr}
+    template <typename feat_t, std::size_t n_feat>
+    float eval_sc(const std::vector<cls_sample<feat_t, n_feat>> & dataset){
+        
+        auto [mj_label, ratio] = majority_label(dataset);
+        
+        return ratio;
+    }
+
+
+    /*
+        ??? split and add nodes ???
+    */
+    template <typename feat_t, std::size_t n_feat>
+    void split(){
+
+    }
+
+
+public:
+
+    /*
+        default constructor
+        threshold: stopping condition threshold
+                   default sc is (#majority_label / #group_size) > threshold
+    */
+    decision_tree(float threshold = 0.95)
+        :   root         {nullptr},
+            n_groups     {0},
+            sc_threshold {threshold}
     {}
 
 
@@ -40,7 +98,66 @@ struct decision_tree{
         builds a decision tree given a dataset (vector of cls_samples)
     */
     template <typename feat_t, std::size_t n_feat>
-    void fit(const std::vector<cls_sample<feat_t, n_feat>> & ds){
+    void fit(const std::vector<cls_sample<feat_t, n_feat>> & dataset){
+
+        auto ds = dataset;
+        std::vector<float> feat_disc(n_feat, 0);
+        
+        // ASSUMING NUMBER OF GROUPS IS SAME FOR ALL FEATURES
+        n_groups = num_groups(ds, 0);
+        
+        // compute discriminative power for each feature
+        for(auto ftr = 0lu; ftr < n_feat; ++ftr)
+            feat_disc[ftr] = disc(ds, ftr);
+        
+        // make list of features from most disc to least
+        std::vector<std::size_t> features;
+        std::size_t              current_feature_idx = 0;
+        auto                     feat_tmp = feat_disc;
+
+        // sort features
+        for(int i = 0; i < feat_tmp.size(); ++i){
+            std::size_t max_index = std::distance(feat_tmp.begin(), std::max_element(feat_tmp.begin(), feat_tmp.end()));
+            features.emplace_back(max_index);
+            feat_tmp[max_index] = 0;
+        }
+
+        // --- SPLITTING ---
+
+        root = new decision_tree_node(BRANCH);
+
+        // add branches for each group (case)
+        for(auto grp_id = 0; grp_id < n_groups; ++grp_id){
+
+            auto grp_ds = get_group_as_ds(ds, features[current_feature_idx], grp_id);
+            
+            // check stopping condition
+            if(eval_sc(grp_ds) > sc_threshold){
+                // time to stop
+
+                auto [ml, _] = majority_label(grp_ds);
+
+                // add a leaf node
+                root->branches.emplace_back(
+                    new decision_tree_node(
+                        LEAF,
+                        features[current_feature_idx],
+                        grp_id,
+                        ml
+                    )
+                );
+
+                // update current feature
+                ++current_feature_idx;
+            }
+            else{
+                // split by current best feature
+
+
+
+                std::cout << "split by current best feature" << std::endl;
+            }
+        }
 
     }
 
@@ -50,6 +167,17 @@ struct decision_tree{
     */
     template <typename feat_t, std::size_t n_feat>
     std::size_t predict(const cls_sample<feat_t, n_feat> & sample){
+
+    }
+
+
+    /*
+        builds a decision tree given a dataset (vector of cls_samples)
+    */
+    template <typename feat_t, std::size_t n_feat>
+    void evaluate(const std::vector<cls_sample<feat_t, n_feat>> & dataset){
+
+
 
     }
 
--- dtree/src/main.cpp
+++ dtree/src/main.cpp
@@ -6,7 +6,10 @@
 #include "parser.hpp"
 #include "dtree.hpp"
 
-#define N_ARGS 1
+#define N_ARGS      1
+#define N_FEATURES  4
+#define N_GROUPS    3
+
 
 using namespace std;
 using namespace dt;
@@ -27,16 +30,20 @@ int main(int argc, char * argv[]){
     auto t1 = std::chrono::high_resolution_clock::now();
 
     parser ps;
-    auto [dataset, id_to_label] = ps.parse_csv<float, 4>(data_filename);
+    auto [dataset, id_to_label] = ps.parse_csv<float, N_FEATURES>(data_filename);
 
     auto t2 = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
     // print class labels
-    for(auto [id, name] : id_to_label) cout << id << ": " << name << endl;
+    cout << "Classes:" << endl;
+    for(auto [id, name] : id_to_label) cout << "  " << id << ": " << name << endl;
 
-    cout << endl << dataset.size() << " samples parsed in " << fixed << setprecision(2) << (float) duration / 1000 << " ms" << endl;
+    auto seconds = (int) std::round(duration / 1000000);
+    auto millis  = (float) (duration % 1000000) / 1000;
+
+    cout << endl << dataset.size() << " samples parsed in " << fixed << setprecision(2) << seconds << " s " << millis << " ms" << endl;
 
 
     // DEBUG
@@ -46,9 +53,28 @@ int main(int argc, char * argv[]){
     cout << "dataset entropy: " << dataset_entropy(dataset) << endl;
 
 
-    // build decision tree
-    
+    // group the features
+    auto ds = discretize_dataset(dataset, N_GROUPS);
 
+    cout << "entropy of group 0 of attribute 0: " << group_entropy(ds, 0, 0) << endl;
+    cout << "entropy of group 1 of attribute 0: " << group_entropy(ds, 0, 1) << endl;
+    cout << "entropy of group 2 of attribute 0: " << group_entropy(ds, 0, 2) << endl;
+
+    cout << "discriminative power of attribute 0: " << disc(ds, 0) << endl;
+    cout << "discriminative power of attribute 1: " << disc(ds, 1) << endl;
+    cout << "discriminative power of attribute 2: " << disc(ds, 2) << endl;
+    cout << "discriminative power of attribute 3: " << disc(ds, 3) << endl;
+
+    // split dataset into training and testing
+    auto [train_ds, test_ds] = split_dataset(ds, 0.8, true);
+
+    // build decision tree
+    decision_tree tree;
+    tree.fit(train_ds);
+
+    // evaluate decision tree
+    auto res = tree.predict(test_ds[0]);
+    cout << "predicted: " << res << "  ground truth: " << test_ds[0].label_id() << endl;
 
     return 0;
 }
